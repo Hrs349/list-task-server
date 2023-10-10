666a6c5bf44308d4f392317ed32b8daa1850c4dc
Humberto Rojas <humbertorojas.349@gmail.com>
2023-10-10 03:15:27 -0400

    avanceproject

---
+++ funtions.js
@@ -0,0 +1,16 @@
+const listaDeTareas = require("./dbList-task");
+const data = listaDeTareas;
+
+function agregarTarea(nuevaTarea) {
+  data.push(nuevaTarea);
+}
+
+function borrarTarea(idTarea) {
+  let lista = data.filter((tarea) => tarea.id !== idTarea);
+  data = lista;
+}
+function imprimirLista() {
+  return data;
+}
+
+module.exports = { agregarTarea, borrarTarea, imprimirLista };
